edbf815460b6011d86cd73362f93b4c1999a2cf8
BIT <BIT@DESKTOP-VPMTOQ5>
2019-08-30 17:03:48 +0900

    # WARNING: head commit changed in the meantime
    dsf

---
+++ src/main/java/kr/co/itcen/network2/EchoClient.java
@@ -0,0 +1,73 @@
+package echo;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.InetSocketAddress;
+import java.net.Socket;
+import java.util.Scanner;
+
+public class EchoClient {
+
+	private static String SERVER_IP = "192.168.1.6";
+	private static int SERVER_PORT = 6000;
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Socket socket = null;
+		
+		try {
+			//1. 소켓생성
+			socket = new Socket();
+
+			//2. 서버연결
+			InetSocketAddress isa = new InetSocketAddress(SERVER_IP, SERVER_PORT);
+			socket.connect(isa);
+			System.out.println("[TCPClient] connected");
+			
+			//3. IOStream 받아오기
+			InputStream is = socket.getInputStream();
+			OutputStream os = socket.getOutputStream();
+			
+			//4. 쓰기
+			Scanner sc = new Scanner(System.in);
+			String data = null;
+			while(true) {
+				System.out.print(">>");
+				data = sc.next();
+				
+				if(!"exit".equals(data))
+					return;
+				
+				os.write(data.getBytes("UTF-8"));
+				
+				//5. 데이터 읽기
+				byte[] buffer = new byte[256];
+				int readByteCount = is.read(buffer);	//읽을게 없으면 blocking
+				
+				if(readByteCount == -1) {				//client가 연결을 끊은 경우
+					// 정상종료 : remote socket이 close()
+					// 메소드를 통해서 정상적으로 소켓을 닫은 경우
+					System.out.println("[TCPClient] closed by server");
+					return;
+				}
+				
+				data = new String(buffer, 0, readByteCount, "UTF-8");
+				System.out.println("<<" + data);
+			}
+			
+			
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} finally {
+			try {
+				if(socket != null && !socket.isClosed())
+					socket.close();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+	}
+}
---
+++ src/main/java/kr/co/itcen/network2/EchoServer.java
@@ -0,0 +1,91 @@
+package echo;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.InetAddress;
+import java.net.InetSocketAddress;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.net.SocketException;
+
+public class EchoServer {
+
+	private static final int PORT = 6000;
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub	    
+
+		ServerSocket serverSocket = null;
+		try {
+			//1. 서버소켓 생성
+			serverSocket = new ServerSocket();		//TCP/IP 설치되어있지 않다면 Exception
+
+			InetAddress ia= InetAddress.getLocalHost();
+
+			//2. Binding
+			//	Socket에 SocketAddress(IPAddress + Port)
+			//	바인딩한다.
+			InetSocketAddress isa = new InetSocketAddress(ia,PORT);
+			if(!serverSocket.isBound())
+				serverSocket.bind(isa);
+			System.out.println("[TCPServer] binding " + ia.getHostAddress() + ":" + PORT);
+
+			//3. Accept
+			//	클라이언트로부터 연결요청(Connect)을 기다린다.(Blocking)
+			Socket socket = serverSocket.accept();		//요청이들어오면 socket 생성
+			InetSocketAddress inetRemoteSocketAddress = (InetSocketAddress)socket.getRemoteSocketAddress();
+			String remoteHostAddress = inetRemoteSocketAddress.getAddress().getHostAddress();
+			int remoteHostPort = inetRemoteSocketAddress.getPort();
+
+			System.out.println("[TCPServer] connected from client[" + remoteHostAddress
+					+ ":" + remoteHostPort + "]");
+
+			try {
+				//4. IOStream 받아오기
+				InputStream is = socket.getInputStream();
+				OutputStream os = socket.getOutputStream();
+
+				while(true) {
+					byte[] buffer = new byte[256];
+					int readByteCount = is.read(buffer);	//읽을게 없으면 blocking
+					System.out.println(readByteCount);
+					if(readByteCount == -1) {				//client가 연결을 끊은 경우
+						// 정상종료 : remote socket이 close()
+						// 메소드를 통해서 정상적으로 소켓을 닫은 경우
+						System.out.println("[TCPServer] closed by client");
+						break;
+					}
+					
+					String data = new String(buffer, 0, readByteCount, "UTF-8");
+					System.out.println("[TCPServer] received :" + data);
+					os.write(data.getBytes());
+				}	//end while
+				
+				//7. 데이터 쓰기
+				
+			} catch(SocketException e) {
+				System.out.println("[TCPServer] closed abnomally");
+			} catch(IOException e) {
+				e.printStackTrace();
+			} finally {
+				if(socket != null && !socket.isClosed())
+					socket.close();
+			}
+
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} finally {
+			//7. 자원정리
+			try {
+				if(serverSocket != null)
+					serverSocket.close();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+
+	}
+
+}
